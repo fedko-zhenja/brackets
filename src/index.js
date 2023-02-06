cee231261c6397e5f8166e8fff645d023175d840
Jhenja Fedko <fedkozhenja@gmail.com>
2023-02-06 06:05:59 +0400

    feat: implement function <brackets>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (string, config) {
+    const newConfig = config.map(item => item.join(''));
+    
+    function findValue () {
+        for (let i = 0; i < newConfig.length; i++) {
+            if (string.includes(newConfig[i]) === true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    while(findValue(string, config)) {
+        for (let i = 0; i < newConfig.length; i++) {
+            string = string.replaceAll(newConfig[i], '');
+        }
+      }
+
+      if (string.length === 0) {
+        return true;
+      } else {
+        return false;
+    }
+
 }
